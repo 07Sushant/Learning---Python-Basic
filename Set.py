67167e16d0da89a31f2ef190fc4c605d35e20fa3
Sushant <93951120+07Sushant@users.noreply.github.com>
2022-11-28 00:16:16 +0530

    Data Collections

---
+++ Set.py
@@ -0,0 +1,12 @@
+from termcolor import colored
+
+# mySet = {"Cars", "boat","train","car"}
+# if "boat" in mySet:
+#     print("boat")
+# else:
+#     print("None")
+
+tuple1 = ("H","E","L","L","O","!")
+
+output ="".join(tuple1)
+print(colored(output,"white","on_red"))
