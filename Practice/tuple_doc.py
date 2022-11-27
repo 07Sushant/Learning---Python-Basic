e3abd60a50a9bd3e86186c5cd01a09495d6b8921
Sushant <93951120+07Sushant@users.noreply.github.com>
2022-11-27 15:24:00 +0530

    Add files via upload

---
+++ Practice/tuple_doc.py
@@ -0,0 +1,53 @@
+# x=(1,2,3)
+# print(len(x))
+
+# tuple = ("Appl̥e")
+# print(tuple)
+
+
+# tuple1 = ("Cars","bike")
+# list1 = list(tuple1)
+# list1.append("Aditya")
+# tuple2 = tuple(list1)
+# print(tuple2)
+
+
+
+
+# tuple1 = (10,20,30,40,50)
+# print(list.reverse(list[tuple]))
+
+
+
+
+# tuple=("Aditya Verma",)
+# print(tuple)
+
+
+
+
+# tuple=(1,2,3,[6,7],4,5)
+# print(tuple[3][0])
+
+
+
+
+# tuple=(1,2,3,[6,7],4,5)
+# print(tuple[3][0],tuple[4])
+
+
+
+
+# tuple=(1,2,3,[6,7],4,5)
+# (tuple[3][0])=8
+# print(tuple)
+
+
+
+
+tuple1=(10,20)
+tuple2=(30,40)
+x=tuple1
+tuple1=tuple2
+print(tuple1)
+print(x)
