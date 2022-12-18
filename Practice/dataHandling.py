ca48600b1919bb57d6abf3f6ef7d3a2186be0d60
Sushant <93951120+07Sushant@users.noreply.github.com>
2022-12-18 16:55:33 +0530

    dataHandling

---
+++ Practice/dataHandling.py
@@ -0,0 +1,15 @@
+class person:
+    def __init__(self,name, age):
+        self.__name = name
+        self.__age = age
+    
+    def getName(self):
+        return self.__name
+
+    def getAge(self):
+        return self.__age
+    
+person1 = person("ABC", 17)
+# print(person1.__name)
+
+print(person1._person__name)
