87ecad41f37a72d6a00eae7cdaadb834f1404d9c
Sushant <93951120+07Sushant@users.noreply.github.com>
2022-12-18 16:54:30 +0530

    inheritance with exceptions

---
+++ Practice/ClassVehicle.py
@@ -0,0 +1,22 @@
+class Vehilce:
+    def capacity():
+        print(4)
+
+
+class car1(Vehilce):
+    pass
+
+class car2(Vehilce):
+    pass
+
+class bus(Vehilce):
+    def capacity():
+        print(50)
+
+objcar1 = car1
+objcar2 = car2
+objbus = bus
+
+objcar1.capacity()
+objcar2.capacity()
+objbus.capacity()
